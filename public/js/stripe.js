cb344ca307fdf0d5558fd8015d97c23726f8c9a1
fadioubari <fadi.oubari@gmail.com>
2021-04-01 01:43:31 +0200

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe(
+  'pk_test_51IarbDD8yMLd9FHKH9ukufdMVpN2ZJBMZO4TOXA1G9huIFEh0tQDMYIylvt7WMunyFH7k1rGxD0rxxNv3BUcGXNJ00462I7FeM'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
