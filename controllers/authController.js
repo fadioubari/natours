bc45001d655c15560feaf6fa84fa03398346185d
fadioubari <fadi.oubari@gmail.com>
2021-04-02 01:35:09 +0200

    fixed req typo in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,7 +58,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
   // 3) If everything ok, send token to client
-  createSendToken(user, 200, rea, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
