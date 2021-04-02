2bb5eb348257f77e899ebcd1fa280b1abfb480a8
fadioubari <fadi.oubari@gmail.com>
2021-04-02 06:39:49 +0200

    stripe new version QA fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -11,25 +11,52 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
 
   // 2) Create checkout session
+  // const session = await stripe.checkout.sessions.create({
+  //   payment_method_types: ['card'],
+  //   // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+  //   //   req.params.tourId
+  //   // }&user=${req.user.id}&price=${tour.price}`,
+  //   success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+  //   cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+  //   customer_email: req.user.email,
+  //   client_reference_id: req.params.tourId,
+  //   line_items: [
+  //     {
+  //       name: `${tour.name} Tour`,
+  //       description: tour.summary,
+  //       images: [
+  //         `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+  //       ],
+  //       amount: tour.price * 100,
+  //       currency: 'usd',
+  //       quantity: 1,
+  //     },
+  //   ],
+  // });
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
+    mode: 'payment',
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
         quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
+          },
+        },
       },
     ],
   });
@@ -51,10 +78,17 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 
+// const createBookingCheckout = async (session) => {
+//   const tour = session.client_reference_id;
+//   const user = (await User.findOne({ email: session.customer_email })).id;
+//   const price = session.display_items[0].amount / 100;
+//   await Booking.create({ tour, user, price });
+// };
+
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
