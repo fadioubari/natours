cac92b960c9228da8e7d92d15a45ef2e0566987f
fadioubari <fadi.oubari@gmail.com>
2021-04-02 04:38:43 +0200

    fixed bookingController bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,12 +2,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
-const catchAsync = require('../utils/catchASync');
+const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1)  Get the currently booked tour
+  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
 
   // 2) Create checkout session
